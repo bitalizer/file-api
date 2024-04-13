54a62dceeaa3f6ac9c28061e7aca516b6a9f4257
Bitalizer <23104115+bitalizer@users.noreply.github.com>
2024-04-13 22:27:24 +0300

    fix: Add @DateTimeFormat to expireTime field in FileUploadRequest
    Add annotation to the expireTime field in the FileUploadRequest class to resolve parsing error for date strings.

--- src/main/kotlin/com/hrblizz/fileapi/data/dtos/FileUploadRequest.kt
+++ src/main/kotlin/com/hrblizz/fileapi/data/dtos/FileUploadRequest.kt
@@ -1,5 +1,6 @@
 package com.hrblizz.fileapi.data.dtos
 
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.web.multipart.MultipartFile
 import java.time.LocalDateTime
 
@@ -8,6 +9,7 @@ data class FileUploadRequest(
     val name: String,
     val contentType: String,
     val source: String,
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     val expireTime: LocalDateTime?,
     val content: MultipartFile,
     val meta: String
